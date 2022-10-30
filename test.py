55e48c890109037cc814b5addcc6e6b830a6cc19
rafifadhila440 <rafifadhila440@gmail.com>
2022-10-30 16:19:19 +0700

    Anggota Kelompok

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("Ini adalah test file")
+print("Anggota Kelompok")
+print("1. Rafi Fadhila Ramadhani")
+print("2. Enrike Manurung")
+print("3. Diana Permatasari")
+print("4. Vella Kharisma Pransisca")
+print("5. Ratu Keisha Laila Rahmadita")
+print("6. Anjelika Maulida")
